2d78a7f930aa56737e334f16f1f70233b208eb91
gianlucafalaschi <gian100gian@outlook.it>
2024-04-11 17:23:33 +0200

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,8 +5,81 @@ export default {
 </script>
 
 <template>
-    Ciao sono l'header
+    <div class="container">
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="logo dc">
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">CHARACTER</a>
+                </li>
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTER</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTER</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTER</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTER</a>
+                </li>
+                <li>
+                    <a href="#">CHARACTER</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+    
 </template>
 
 <style scoped lang="scss">
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
+        padding-top:15px;
+        padding-bottom:15px;
+
+        .logo {
+            width: 20%;
+            img {
+                max-width: 100px;
+            }
+        }
+
+        nav{
+            width: 80%;
+            ul {
+                width: 100%;
+                display: flex;
+                justify-content: flex-end;
+                flex-wrap: wrap;
+                gap: 20px;
+                font-size: 16px;
+                li {
+                    a {
+                        color: white; 
+                    }
+                } 
+                
+            }
+        } 
+    } 
 </style>
